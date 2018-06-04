e9dafcd903bd7016d4eba6bafcb08e03996e5e7d
Mark Barretto <mark.barretto@gmail.com>
2018-06-04 21:44:10 +0800

    used toggle function

--- timer/timer.ino
+++ timer/timer.ino
@@ -6,31 +6,31 @@ void setup() {
   Serial.begin(9600);
 }
 
-void switchOn() {
+void switchOn(int pin) {
   return digitalWrite(lightPin, HIGH);
 }
 
-void switchOff() {
+void switchOff(int pin) {
   return digitalWrite(lightPin, LOW);
 }
 
-void toggleMister() {
-  //do something
+void togglePin(int pin) {
+  checkAtInterval(1000, switchOn, pin); // turn on after 1 sec
+  checkAtInterval(2000, switchOff, pin); // turn off after 2 secs
 }
 
-void checkAtInterval(int timeInterval, void (&callMethod)(void)) {
-  long timeSet = millis() + timeInterval;
+void checkAtInterval(int timeInterval, void (&callMethod)(int), int pin) {
+  unsigned long timeSet = millis() + timeInterval;
 
-  while(true) {
-    if(timeSet == millis()) {
-      callMethod();
+  while (true) {
+    if (timeSet == millis()) {
+      callMethod(pin);
       break;
-    }  
+    }
   }
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  checkAtInterval(1000, switchOn); // turn on after 1 sec
-  checkAtInterval(2000, switchOff); // turn off after 2 secs
+  togglePin(13);
 }
